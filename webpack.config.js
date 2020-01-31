dc437214a41ddc49c71840fbdaf58308968f2e8f
indieasysoftware <indieasy.software@gmail.com>
2020-01-31 01:09:07 -0500

    more package changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-	entry: './index.js',
+	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'build'),
 		filename: 'index.js',
