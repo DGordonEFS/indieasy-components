dcfad478e3dd7ab86de6361f7c41e8cac79f8bd2
indieasysoftware <indieasy.software@gmail.com>
2020-02-25 12:47:36 -0500

    tooltip store.getStore().getState() => state

--- src/hoc/withToolTip/withToolTip.js
+++ src/hoc/withToolTip/withToolTip.js
@@ -1,10 +1,8 @@
 import { connect } from 'react-redux';
-import { store } from '@indieasy.software/indieasy-engine';
 
 const mapStateToProps = (state) => {
-	console.log(store);
 	return {
-		toolTip: store.getStore().getState().tooltips.currentToolTip,
+		toolTip: state.tooltips.currentToolTip,
 	};
 };
 
