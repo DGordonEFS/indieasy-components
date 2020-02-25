18998c4333243adf85dd20bfd3f45431ade060bb
indieasysoftware <indieasy.software@gmail.com>
2020-02-25 11:41:30 -0500

    components and hoc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // Export all the explicitly exported components, this file will contain our
 // components when built by webpack and sent off to the world.
 
-export * from './components';
-export * from './hoc';
+import * as components from './components';
+import * as hoc from './hoc';
+
+export { components, hoc };
 //
