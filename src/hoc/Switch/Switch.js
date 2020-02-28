de8fa8daf6d9ff227724dc8869125d3689cd566a
indieasysoftware <indieasy.software@gmail.com>
2020-02-28 09:36:50 -0500

    switch fix

--- src/hoc/Switch/Switch.js
+++ src/hoc/Switch/Switch.js
@@ -1,20 +1,20 @@
 import React, { Fragment } from 'react';
 
-const Switch = props => {
-  const child = props.children.find(item => {
-    if (item.props.isValid) return item.props.isValid();
-    else {
-      return (
-        item.props.target === undefined || props.value === item.props.target
-      );
-    }
-  });
+const Switch = (props) => {
+	const child = React.Children.toArray(props.children).find((item) => {
+		if (item.props.isValid) return item.props.isValid();
+		else {
+			return (
+				item.props.target === undefined || props.value === item.props.target
+			);
+		}
+	});
 
-  return <Fragment>{child}</Fragment>;
+	return <Fragment>{child}</Fragment>;
 };
 
 export default Switch;
 
-export const SwitchItem = props => {
-  return <Fragment>{props.children}</Fragment>;
+export const SwitchItem = (props) => {
+	return <Fragment>{props.children}</Fragment>;
 };
